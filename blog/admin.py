030189e62d5425ddfdb8538c686e7d2169ff57a8
Kamil <kmirosz6@gmail.com>
2026-04-10 09:41:38 +0200

    Unregister Group model from admin to streamline admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from blog.models import Commentary, Post
+
+admin.site.unregister(Group)
 
 
 # Register your models here.
