32ce51609f86cd1422f379236f44162cf4705aad
Kamil <kmirosz6@gmail.com>
2026-04-10 09:05:05 +0200

    [Green] Add Commentary model and update Post model's owner relationship

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,23 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="posts"
+    )
 
     def __str__(self) -> str:
         return self.title
+
+
+class Commentary(models.Model):
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="coments"
+    )
+    post = models.ForeignKey(
+        to=Post, on_delete=models.CASCADE, related_name="coments"
+    )
+
+    def __str__(self) -> str:
+        return self.content
