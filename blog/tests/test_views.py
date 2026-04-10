b93a77f5c5e9524923b62e07a7f21a1e96d5eb3e
Kamil <kmirosz6@gmail.com>
2026-04-10 10:22:53 +0200

    [Red] Enhance IndexTestCase with additional posts and pagination test

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -23,6 +23,11 @@ class IndexTestCase(TestCase):
             title="Title3", content="Body3", owner=self.user
         )
 
+        for i in range(4, 11):
+            Post.objects.create(
+                title=f"Title{i}", content=f"Body{i}", owner=self.user
+            )
+
     def test_view_returns_200(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
@@ -33,3 +38,16 @@ class IndexTestCase(TestCase):
             list(response.context["posts"]),
             list(Post.objects.all().order_by("-created_time")),
         )
+
+    def test_view_paginated_correctly(self):
+        response = self.client.get(self.url)
+        self.assertEqual(len(response.context["posts"]), 5)
+
+    def test_view_pagination_page_2(self):
+        response = self.client.get(self.url, {"page": 2})
+        self.assertEqual(len(response.context["posts"]), 5)
+        self.assertEqual(response.context["posts"][0].title, "Title6")
+        self.assertEqual(response.context["posts"][1].title, "Title7")
+        self.assertEqual(response.context["posts"][2].title, "Title8")
+        self.assertEqual(response.context["posts"][3].title, "Title9")
+        self.assertEqual(response.context["posts"][4].title, "Title10")
