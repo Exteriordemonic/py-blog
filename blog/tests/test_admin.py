ba947f9f9e9e780afdce8fdbf656c065d05ba8a3
Kamil <kmirosz6@gmail.com>
2026-04-10 09:22:24 +0200

    [RED] Add tests for Commentary model in admin view

--- blog/tests/test_admin.py
+++ blog/tests/test_admin.py
@@ -4,6 +4,7 @@ from django.shortcuts import reverse
 
 
 from blog.models import Post
+from blog.models import Commentary
 
 
 class PostAdminTestCase(TestCase):
@@ -29,5 +30,34 @@ class PostAdminTestCase(TestCase):
     def test_admin_add_view(self):
         url = reverse("admin:blog_post_add")
 
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+class CommentaryAdminTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_superuser(
+            username="admin", password="adminpass", email="admin@example.com"
+        )
+        self.author = get_user_model().objects.create_user(
+            username="Author1", password="password1"
+        )
+        self.post = Post.objects.create(
+            title="Title1", content="Body1", owner=self.author
+        )
+        self.client.force_login(self.user)
+        self.commentary = Commentary.objects.create(
+            content="BodyCommentary1", user=self.author, post=self.post
+        )
+
+    def test_admin_changelist_view(self):
+        url = reverse("admin:blog_commentary_changelist")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "BodyCommentary1")
+        self.assertContains(response, "Author1")
+
+    def test_admin_add_view(self):
+        url = reverse("admin:blog_commentary_add")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
