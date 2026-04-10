ac055d50386a7e87100ff4f4e5bce4624e8d9b5b
Kamil <kmirosz6@gmail.com>
2026-04-10 11:38:02 +0200

    [Refactor] Update middleware configuration for debug toolbar and fix context variable in PostListTest

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from blog.models import Post, Commentary
+from blog.models import Post
 
 MAIN_PAGE_URL = reverse("blog:index")
 PAGINATION = 5
@@ -19,12 +19,12 @@ class PostListTest(TestCase):
     def test_main_page_paginated_correctly(self):
         response = self.client.get(MAIN_PAGE_URL)
 
-        self.assertEqual(len(response.context["post_list"]), PAGINATION)
+        self.assertEqual(len(response.context["posts"]), PAGINATION)
 
     def test_main_page_ordered_by_created_time(self):
         response = self.client.get(MAIN_PAGE_URL)
         post_list = Post.objects.all().order_by("-created_time")
-        post_context = response.context["post_list"]
+        post_context = response.context["posts"]
 
         self.assertEqual(
             list(post_context),
