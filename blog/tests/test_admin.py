7b6b624c7f3cf12ed9367b5041f0a245eddd7727
Kamil <kmirosz6@gmail.com>
2026-04-10 09:14:30 +0200

    [RED] Create test for admin view of post model

---
+++ blog/tests/test_admin.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.shortcuts import reverse
+
+
+from blog.models import Post
+
+
+class PostAdminTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_superuser(
+            username="admin", password="adminpass", email="admin@example.com"
+        )
+        self.author = get_user_model().objects.create_user(
+            username="Author1", password="password1"
+        )
+        self.client.force_login(self.user)
+        self.post = Post.objects.create(
+            title="Title1", content="Body1", owner=self.author
+        )
+
+    def test_admin_changelist_view(self):
+        url = reverse("admin:post_changelist")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Title1")
+        self.assertContains(response, "Author1")
+
+    def test_admin_add_view(self):
+        url = reverse("admin:blog_post_add")
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
